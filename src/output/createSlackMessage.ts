3b0c286b1a8d2bfdf3894eafb82d2180ebdbe246
Adam Kelsall <adam.kelsall25@gmail.com>
2017-07-24 00:36:43 +0100

    Message when the number of disruptions changes (#5)

--- src/output/createSlackMessage.ts
+++ src/output/createSlackMessage.ts
@@ -1,18 +1,23 @@
 import * as moment from "moment-timezone";
 import { Moment } from "moment-timezone";
+import * as pluralize from "pluralize";
 
 import { SlackMessage, SlackMessageAttachment } from ".";
 import { Disruption, formatDisruption } from "../input";
 
-export function createSlackMessage(disruptions: Cheerio): SlackMessage {
-  const formattedDisruptions: Disruption[] = [];
-
-  disruptions.each((index, disruption) => {
-    formattedDisruptions.push(formatDisruption(disruption));
-  });
-
+export function createSlackMessage(previousCount: number, disruptions: Cheerio): SlackMessage {
   const message: SlackMessage = {
-    attachments: formattedDisruptions.map((disruption, index) => {
+    text: textForCount(previousCount, disruptions.length),
+  };
+
+  if (disruptions.length) {
+    const formattedDisruptions: Disruption[] = [];
+
+    disruptions.each((index, disruption) => {
+      formattedDisruptions.push(formatDisruption(disruption));
+    });
+
+    message.attachments = formattedDisruptions.map((disruption, index) => {
       const lines = disruption.lines.join("\n");
       const updated: Moment = moment(disruption.updated);
       const time: string = updated.format("H:mma");
@@ -25,9 +30,35 @@ export function createSlackMessage(disruptions: Cheerio): SlackMessage {
       };
 
       return attachment;
-    }),
-    text: "One or more disruptions have been detected following a period where there were none :frowning:",
-  };
+    });
+  }
 
   return message;
+}
+
+function textForCount(previousCount: number, count: number): string {
+  if (!count) {
+    return `There are no longer any disruptions! ${emojiForCount(count)}`;
+  }
+
+  const pluralAre = pluralize("is", count);
+  const pluralDisruption = pluralize("disruption", count);
+  const direction = previousCount < count ? "up" : "down";
+  const emoji = emojiForCount(count);
+
+  return `There ${pluralAre} now ${count} ${pluralDisruption}, ${direction} from ${previousCount} ${emoji}`;
+}
+
+function emojiForCount(count: number): string {
+  const emojis = [
+    ":smile: :tada:",
+    ":worried:",
+    ":anguished:",
+    ":persevere:",
+    ":cold_sweat:",
+    ":scream:",
+    ":upside_down_face:",
+  ];
+
+  return count > 5 ? emojis[6] : emojis[count];
 }
