bbdb0056ab3de2f75e0322c93d38d1c66ecfbd32
adamkelsall <adam.kelsall25@gmail.com>
2017-07-25 23:00:54 +0100

    mongo connect

--- src/persist/mongoose.ts
+++ src/persist/mongoose.ts
@@ -2,12 +2,13 @@ import * as mongoose from "mongoose";
 
 import { Disruption } from ".";
 
-(<any>mongoose).Promise = Promise;
-
 // export async function storeUniqueDisruption() {
   
 // }
 
 export async function getDisruptionByText(text: string) {
+  (<any>mongoose).Promise = Promise;
+  await mongoose.connect("localhost:27017/slack-metro");
+
   return await Disruption.findOne({ text })
 }
